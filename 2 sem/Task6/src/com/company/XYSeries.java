c83cd7bdfecc9a66d082b81d9b111740ac023ac4
KennyDied <thekennydied@mail.ru>
2020-05-19 21:56:16 +0300

    123123

--- 2 sem/Task6/src/com/company/XYSeries.java
+++ 2 sem/Task6/src/com/company/XYSeries.java
@@ -4,6 +4,7 @@ public class XYSeries {
     public static XYSeries getMapCmpLine(Experiment info){
         XYSeries series = new XYSeries();
         int size = info.getSize();
-
+        for (int i = 0; i < size; i++) {
+        }
     }
 }
