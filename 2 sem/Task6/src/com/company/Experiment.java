0bbd1fe5ad7a8e0096158b5228be4a2b819660ca
KennyDied <thekennydied@mail.ru>
2020-06-02 15:16:19 +0300

    сдал 6

--- 2 sem/Task6/src/com/company/Experiment.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Experiment {
-    private static Integer[] generate(int size) {
-        Integer [] array = new Integer[size];
-        //заполнить массив случайными числами
-        return array;
-    }
-}
